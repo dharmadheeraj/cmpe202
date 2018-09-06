2afe4156917090e15edb53de925bbf84dd724222
Dharma Dheeraj Chintala <38597753+dharmadheeraj@users.noreply.github.com>
2018-09-05 23:20:21 -0700

    used constructor overloading and defined additional method params
    used constructor overloading and defined additional method params

--- Lab1/gumball-java-typical/GumballMachine.java
+++ Lab1/gumball-java-typical/GumballMachine.java
@@ -1,43 +1,83 @@
-
 public class GumballMachine
 {
 
     private int num_gumballs;
-    private boolean has_quarter;
+    private boolean has_coin;
+	private int value;
+	private int accepted;
+	private boolean valid_coin;
+	private int sum;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size , int value)
     {
         // initialise instance variables
         this.num_gumballs = size;
-        this.has_quarter = false;
+		this.value = value;
+        this.has_coin = false;
+        this.sum = 0;
     }
 
-    public void insertQuarter(int coin)
+	public GumballMachine( int size , int value, int accepted)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        // initialise instance variables
+        this.num_gumballs = size;
+		this.value = value;
+		this.accepted = accepted;
+		this.valid_coin = false;
+        this.has_coin = false;
+        this.sum = 0;
+    }
+	
+	
+    public void insertQuarter(int coin,GumballMachine GM)
+    {
+        if(GM.accepted > 0 )
+        {
+        	if(GM.accepted == coin)
+        	{
+	        	GM.has_coin = true;
+	        	GM.valid_coin = true;
+	        	GM.sum = GM.sum + coin;
+        	}
+        	else
+        	{
+        		GM.has_coin = true;
+        		GM.valid_coin = false;
+        		GM.sum = GM.sum + coin;
+        	}
+        }
+        else
+        {
+        	GM.has_coin = true;
+    		GM.valid_coin = true;
+    		GM.sum = GM.sum + coin;
+        }
     }
     
-    public void turnCrank()
+    public void turnCrank(GumballMachine GM)
     {
-    	if ( this.has_quarter )
+    	if ( GM.has_coin && GM.value <= GM.sum && GM.valid_coin)
     	{
-    		if ( this.num_gumballs > 0 )
+    		if ( GM.num_gumballs > 0 )
     		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+    			GM.num_gumballs-- ;
+    			GM.has_coin = false ;
+    			System.out.println( "Thanks for your amount.  Gumball Ejected!" ) ;
+    			GM.sum = GM.sum - GM.value;
     		}
     		else
     		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+    			System.out.println( "No More Gumballs!  Sorry, can't return your amount." ) ;
     		}
     	}
     	else 
     	{
-    		System.out.println( "Please insert a quarter" ) ;
+    		if(!GM.valid_coin)
+    		System.out.println( "Please insert a Valid Coin" ) ;
+    		if(!GM.has_coin)
+    		System.out.println( "Please insert a Coin" ) ;
+    		if(GM.value > GM.sum)
+    		System.out.println( "Please insert a Sufficient Funds" ) ;
     	}        
     }
 }
--- Lab1/gumball-java-typical/Main.java
+++ Lab1/gumball-java-typical/Main.java
@@ -1,21 +1,19 @@
-
-
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		GumballMachine gumballMachine = new GumballMachine(5,50,25);
 
 		System.out.println(gumballMachine);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter( 25 ,gumballMachine );
+		gumballMachine.turnCrank(gumballMachine);
 
 		System.out.println(gumballMachine);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		gumballMachine.insertQuarter( 25 ,gumballMachine );
+		gumballMachine.turnCrank(gumballMachine);
+		gumballMachine.insertQuarter( 50 ,gumballMachine );
+		gumballMachine.turnCrank(gumballMachine);
 
 		System.out.println(gumballMachine);
 	}
